c4c72ec35623223467d48450e709a90ac822d185
Aleksander Gnat <aleksander.gnat@pw.edu.pl>
2024-09-19 16:08:29 +0200

    Works

--- main_2.py
+++ main_2.py
@@ -24,40 +24,56 @@ msp.add_circle((0,0), diameter_overall/2)
 diameter_pattern = 1
 distance = 2
 
-centers_xy = {(0,0)}
-outers_xy = {(0,0)}
-
 def hexagon(cx0,cy0,distance,index,color):
     plt.text((cx0), (cy0), f'{index}', ha='center', va='center', color=color)
-    print(f'Hex: {index} at x:{cx0} | cy:{cy0} creates:')
+    outers_xy = set()
     for i in range(6):
         cx = Decimal(cx0) + Decimal(math.cos(math.pi / 3 * i) * distance).quantize(Decimal('.00001'))
         cy = Decimal(cy0) + Decimal(math.sin(math.pi / 3 * i) * distance).quantize(Decimal('.00001'))
-        centers_xy.add((Decimal(cx), Decimal(cy)))
         outers_xy.add((Decimal(cx), Decimal(cy)))
         plt.text((cx), (cy), f'{index}:{i}', ha='center', va='center', color=color)
-        print(f'SubHex: {i} at x:{cx} | cy:{cy} creates:')
-    print('New hex incoming...')
+    return outers_xy
 colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'brown', 'pink']
-hexagon(0,0,distance,0,'black')
-outers_xy.remove((0,0))
-
-prev_outers_xy = outers_xy
-print(prev_outers_xy)
-for j in range(len(list(prev_outers_xy))):
-     cx0, cy0 = list(prev_outers_xy)[j]
-     print(f'cx0:{cx0} | cy0:{cy0}')
-     for i in range(6):
-         cx = Decimal(cx0) + Decimal(math.cos(math.pi / 3 * i) * distance).quantize(Decimal('.00001'))
-         cy = Decimal(cy0) + Decimal(math.sin(math.pi / 3 * i) * distance).quantize(Decimal('.00001'))
-         plt.text((cx), (cy), f'{j}:{i}', ha='center', va='center', color='red')
 
 
+cx0,cy0 = 0,0
+centers_set = hexagon(cx0, cy0, distance, 0, 'black')
+
+
+centers_list = list(centers_set)
+old_centers_set = centers_set
+old_centers_list = list(old_centers_set)
+
+
+test = set()
+for i in range(len(old_centers_list)):
+    print('-')
+    print(len(old_centers_list))
+    cx0,cy0 = old_centers_list[i]
+    print(f'cx{cx0} : cy{cy0}')
+    print(len(centers_set))
+    print(len(old_centers_set))
+    test.update(hexagon(cx0, cy0, distance, 0, colors[i]))
 
 
 
+y = test - centers_set
+test = set()
+for i in range(len(list(y))):
+    cx0, cy0 = list(y)[i]
+    test.update(hexagon(cx0, cy0, distance, 0, 'black'))
 
+y = test - centers_set
+test = set()
+for i in range(len(list(y))):
+    cx0, cy0 = list(y)[i]
+    test.update(hexagon(cx0, cy0, distance, 0, 'red'))
 
+y = test - centers_set
+test = set()
+for i in range(len(list(y))):
+    cx0, cy0 = list(y)[i]
+    test.update(hexagon(cx0, cy0, distance, 0, 'red'))
 
 plt.show()
-doc.saveas('circle.dxf')
+#doc.saveas('circle.dxf')
---
+++ main_3.py
@@ -0,0 +1,90 @@
+import ezdxf
+import math
+from decimal import Decimal, ROUND_DOWN
+import matplotlib.pyplot as plt
+
+
+# Plot to see what's happening
+size = 20
+plt.figure(figsize=(size, size))
+plt.title('Circle Pattern')
+plt.xlim(-size, size)
+plt.ylim(-size, size)
+
+# Create doc object
+doc = ezdxf.new('R2010',units=ezdxf.units.MM)
+
+# Create model space
+msp = doc.modelspace()
+# Add a new layer
+layer_pattern = "Pattern"
+doc.layers.add(layer_pattern)
+layer_shape = "Shape"
+doc.layers.add(layer_shape)
+layer_hole = "Hole"
+doc.layers.add(layer_hole)
+
+# Parameter of the overall shape
+diameter_overall = 30
+
+
+# Parameter of the pattern
+diameter_pattern = 1
+distance = 2
+
+# Hole parameters
+hole = 2
+spacing = 25
+
+circles = []
+def hexagon(cx0,cy0,distance):
+    #plt.text((cx0), (cy0), f'O', ha='center', va='center', color='red')
+    outers_xy = set()
+    for i in range(6):
+        cx = Decimal(cx0) + Decimal(math.cos(math.pi / 3 * i) * distance).quantize(Decimal('.00001'))
+        cy = Decimal(cy0) + Decimal(math.sin(math.pi / 3 * i) * distance).quantize(Decimal('.00001'))
+        outers_xy.add((Decimal(cx), Decimal(cy)))
+    return outers_xy
+
+def outside_shape(diameter, spacing, hole):
+
+    plt.gca().add_artist(plt.Circle((0, 0), radius=diameter / 2, fill=False, linewidth=1, edgecolor='blue'))
+    msp.add_circle((0, 0), diameter / 2, dxfattribs={"layer": layer_shape})
+    plt.gca().add_artist(plt.Circle((0, spacing/2), radius=hole / 2, fill=False, linewidth=1, edgecolor='green'))
+    plt.gca().add_artist(plt.Circle((0, -spacing / 2), radius=hole / 2, fill=False, linewidth=1, edgecolor='green'))
+    msp.add_circle((0, spacing/2), hole / 2, dxfattribs={"layer": layer_hole})
+    msp.add_circle((0, -spacing / 2), hole / 2, dxfattribs={"layer": layer_hole})
+    return True
+
+
+all_centers = set()
+centers = set()
+centers.update(hexagon(0, 0, distance))
+all_centers.update(centers)
+new_centers = set()
+iteration_centers = centers
+
+
+for j in range(4):
+
+    for center in iteration_centers:
+        cx0,cy0 = list(center)
+        new_centers.update(hexagon(cx0, cy0, distance))
+        all_centers.update(new_centers)
+    iteration_centers = new_centers - centers
+
+
+print(len(all_centers))
+for center in range(len(all_centers)):
+    cx0,cy0 = list(all_centers)[center]
+    print(f'cx:{cx0} | cy:{cy0}')
+    #plt.text((cx0), (cy0), 'O', ha='center', va='center', color='red')
+    plt.gca().add_artist(plt.Circle((cx0,cy0), radius=diameter_pattern/2, fill=False, linewidth=1, edgecolor='red'))
+    msp.add_circle((cx0, cy0), diameter_pattern / 2,dxfattribs={"layer": layer_pattern})
+
+
+
+outside_shape(diameter_overall, spacing, hole)
+
+plt.show()
+doc.saveas('circle.dxf')
